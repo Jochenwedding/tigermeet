da9ff63ab80fb781793c1cb69c96200dd8fb8ff9
Jochenwedding <michels.jochen10@gmail.com>
2026-05-15 13:48:56 +0200

    Update soundboard.js

--- functions/soundboard.js
+++ functions/soundboard.js
@@ -1,9 +1,1 @@
-export async function onRequest() {
-  return new Response("404 Not Found", {
-    status: 404,
-    headers: {
-      "Content-Type": "text/plain",
-      "Cache-Control": "no-store, no-cache, must-revalidate, max-age=0"
-    }
-  });
-}
+
